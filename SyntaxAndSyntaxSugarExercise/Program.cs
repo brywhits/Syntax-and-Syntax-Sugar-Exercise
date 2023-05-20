b3cbacbe4a80ffbaf395b781bf4cc4ea58bc6b13
Bryant Whitsett <bryantwhitsett71@gmail.com>
2023-05-19 21:47:49 -0400

    Completed Syntax

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,16 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Conditions
+            var answer = 4;
+            var response = (answer < 9)? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
+
+
+
         }
     }
 }
